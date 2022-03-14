fe79cd1c460f0aec9f10e4d1ea4d2e057c703e5d
dheeraj <dheerajm@prdxn.com>
2022-03-14 17:49:52 +0530

    feat: first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    {/* <nav className="px-2 text-md font-bold bg-slate-800 text-slate-200">
+      <ul className='flex'>
+        <li className='mx-2 my-2'>Home</li>
+        <li className='mx-2 my-2'>Options</li>
+        <li className='mx-2 my-2'>About</li>
+        <li className='mx-2 my-2'>Contact Us</li>
+      </ul>
+    </nav> */}
+    {/*  */}
+    <div className='content'>
+      <div className='one'>1</div>
+      <div className='two'>2</div>
+      <div className='three'>3</div>
+    </div>
+    </>
+  );
+}
+
+export default App;
